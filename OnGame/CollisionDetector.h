edbb2a3bd7062cb1c552560f028163d1927472e2
moondory77 <moondory77@naver.com>
2018-10-24 18:04:46 +0900

    블록유닛 chunk 단위로 충돌처리하도록 개선

--- OnGame/CollisionDetector.h
+++ OnGame/CollisionDetector.h
@@ -5,12 +5,38 @@
 USING_NS_CC;
 using namespace std;
 
-//충돌감지를 위한, 분리축 정보를 담는 데이터 클래스
+//(1차원 상에서) 두 segment에 대해, overlap 범위를 반환 (각 pair는 segment의 <min, max>) 
+static inline pair<float, float> isOneDimensionOverlap(const pair<float, float>& AB, const pair<float, float>& CD)
+{
+	float minAB = AB.first;
+	float maxAB = AB.second;
+	float minCD = CD.first;
+	float maxCD = CD.second;
+
+	if (maxAB < minCD || minAB > maxCD) {
+		return{ 0, 0 };
+	}
+	else {
+		//AB를 right 방향 맨 끝에서 left로 조금씩 이동시킨다 생각하며 overlap 측정
+		if (minCD <= minAB && minAB <= maxCD) {
+			return{ minAB ,  (maxAB < maxCD ? maxAB : maxCD) };
+		}
+		else if (minCD <= maxAB && maxAB <= maxCD) {
+			return{ minCD , maxAB };
+		}
+		else {
+			return{ minCD , maxCD };
+		}
+	}
+}
+
+
+//충돌 감지를 위한, 분리축 정보를 담는 데이터 클래스
 class SeparateAxis : public CCNode
 {
-	Vec2 normAxis;			//(초기화 시, 입력받은 vector의 노멀벡터) 분리축
-	float projMinTarget;	//(축에 projection 된 값 중) 최솟값
-	float projMaxTarget;	//(축에 projection 된 값 중) 최댓값
+	Vec2 normAxis;			//(초기화 시, 입력받은 vector의 노멀벡터) 분리축 벡터
+	float projMinTarget;	//(현재 target에 대해, projection 된 값 중) 최솟값
+	float projMaxTarget;	//(현재 target에 대해, projection 된 값 중) 최댓값
 
 public:
 
@@ -20,70 +46,104 @@ public:
 	void setProjMin(float min) { projMinTarget = min; }
 	void setProjMax(float max) { projMaxTarget = max; }
 
-	float getProjMinTarget() { return projMinTarget; }
-	float getProjMaxTarget() { return projMaxTarget; }
-
+	float getProjMin() { return projMinTarget; }
+	float getProjMax() { return projMaxTarget; }
 	Vec2 getNormAxis() { return normAxis; }
 
-
-	//입력받은 vector에 대해, normal-vector를 구해 분리축을 설정
+	//perpendicular normal vector (start -> dst)
 	inline void initAxis(Vec2 start, Vec2 dst) {
 		this->normAxis = (dst - start).getPerp().getNormalized();
 		projMinTarget = projMaxTarget = 0.0f;
 	}
+
 	//(임의의 점을) 해당 축에 projection 시킨 값을 반환
 	inline float getProjection(Vec2 input_vec) { return normAxis.dot(input_vec); }
 
-	inline float calOverlapped(Vec2 node_pos, float node_radius)
+	//해당 축에 대한, 충돌체의 penetration 구간 (min, max)
+	inline pair<float, float> getPenetrationBound(Vec2 invade_direction, Vec2 center_pos, float radius)
 	{
-		float proj_val = this->getProjection(node_pos);	//해당 축으로 projection
-		float proj_min = proj_val - node_radius;
-		float proj_max = proj_val + node_radius;
-		float overlap_val = 0.0f;
+		float invade_center = getProjection(center_pos);
+		float invade_min = invade_center - radius;
+		float invade_max = invade_center + radius;
 
-		//(projection 된 값 중) 최솟값이 타겟 정사영 범위에 속함
-		if (getProjMinTarget() < proj_min && proj_min < getProjMaxTarget())
-		{
-			float max_clamp = proj_max < getProjMaxTarget() ? proj_max : getProjMaxTarget();
-			overlap_val = max_clamp - proj_min;
+		if (invade_max < projMinTarget || invade_min > projMaxTarget) {
+			return{ 0, 0 };
 		}
-		//(projection 된 값 중) 최댓값이 타겟 정사영 범위에 속함
-		else if (getProjMinTarget() < proj_max && proj_max < getProjMaxTarget())
-		{
-			float min_clamp = proj_min > getProjMinTarget() ? proj_min : getProjMinTarget();
-			overlap_val = proj_max - min_clamp;
+		else {
+			if (getProjection(invade_direction) < 0)
+				return{ projMaxTarget , invade_min };
+			else
+				return{ projMinTarget, invade_max };
+		}
+	}
+	inline pair<float, float> getPenetrationBound(Vec2 invade_direction, vector<Vec2>& invade_vertices)
+	{
+		auto iter = invade_vertices.begin();
+		float cur_val = getProjection(*iter);
+		pair<float, float> invade_bound = { cur_val , cur_val };
+
+		while (++iter != invade_vertices.end()) {
+			cur_val = getProjection(*iter);
+			if (cur_val < invade_bound.first)
+				invade_bound.first = cur_val;
+			if (cur_val > invade_bound.second)
+				invade_bound.second = cur_val;
 		}
 
-		return overlap_val;
-	};
+		if (invade_bound.second < projMinTarget || invade_bound.first > projMaxTarget) {
+			return{ 0, 0 };
+		}
+		else {
+			if (getProjection(invade_direction) < 0)
+				return{ projMaxTarget , invade_bound.first };
+			else
+				return{ projMinTarget , invade_bound.second };
+		}
+	}
+
+	//해당 축에 대한, penetration depth 
+	inline float getPenetrationDepth(Vec2 invade_direction_vec, Vec2 center_pos, float radius)
+	{
+		pair<float, float> p_bound = getPenetrationBound(invade_direction_vec, center_pos, radius);
+		return p_bound.second - p_bound.first;
+	}
+	inline float getPenetrationDepth(Vec2 invade_direction_vec, vector<Vec2>& invade_vertices)
+	{
+		pair<float, float> p_bound = getPenetrationBound(invade_direction_vec, invade_vertices);
+		return p_bound.second - p_bound.first;
+	}
 };
-
 
 
 class CollisionDetector : public Layer
 {
-	const Sprite& target;
+	Sprite* target;
+	
+	/*부딪쳐 오는 object에 대해, 상대속도의 방향으로 분리축의 방향 결정 시퀀스 삽입 필요
+	///target의 현재 위치와 이전 프레임 위치 추적 필요*/
 
-	AutoPolygon* tPolygon;
 	DrawNode* tLiner;
+	AutoPolygon* tPolygon;
 	vector<SeparateAxis*> tAxises = {};
 
-	///vertex는, scaling이 안된 이미지 원본상태에서의 좌표값
 	vector<Vec2> protoVertices;			//텍스쳐 상의 vertex container
 	vector<Vec2> curVertices;			//reduce 시킨 후, vertices 저장 -> 충돌체크 대상 vertex	
+
 	float reduceEpsilon;
-	float scaleFactor;		//(타겟 스프라이트에 대해) 콜라이더의 스케일 비율
+	float scaleFactor;					//(타겟 스프라이트에 대해) 콜라이더의 스케일 비율
 
 public:
 
-	///filename은 target과 동일한 스프라이트여야 한다
-	CollisionDetector(const string& filename, Sprite* spr_target, float scale, float reduce_epsilon) : target(*spr_target)
+	///Target으로 할 스프라이트와 동일한 파일주소 삽입
+	CollisionDetector(const string& filename, Sprite* spr_target, float scale, float reduce_epsilon)
+		: target(spr_target)
 	{
+		tPolygon = new AutoPolygon(filename);
 		tLiner = DrawNode::create();
 		tLiner->retain();
 		this->addChild(tLiner);
-		tPolygon = new AutoPolygon(filename);
-		protoVertices = tPolygon->trace(Rect(0, target.getContentSize().height / 2, target.getContentSize().width, target.getContentSize().height / 2));
+
+		protoVertices = tPolygon->trace(Rect(0, 0, target->getContentSize().width, target->getContentSize().height));
 		scaleFactor = scale;
 		setEpsilon(reduce_epsilon);
 	};
@@ -97,7 +157,6 @@ public:
 		delete(tPolygon);
 	};
 
-
 	const vector<Vec2>& getVertices() { return curVertices; };
 
 	inline float getEpsilon() { return this->reduceEpsilon; }
@@ -109,24 +168,22 @@ public:
 	///epsilon값에 의해, 외곽선을 다시 트랙킹한 후, 분리축 데이터 초기화
 	inline void initAxises() {
 		{
-			curVertices = tPolygon->reduce(protoVertices, Rect(0, target.getContentSize().height / 2, target.getContentSize().width, target.getContentSize().height / 2), reduceEpsilon);
+			curVertices = tPolygon->reduce(protoVertices, Rect(0, 0, target->getContentSize().width, target->getContentSize().height), reduceEpsilon);
 
-			///sprite보다 약간 더 넓게 콜라이더 면적 적용(충돌처리 부드럽게하기위해)
 			for (auto iter = curVertices.begin(); iter != curVertices.end(); iter++) {
-				*iter -= Vec2(target.getContentSize().width / 2, 0);
+				*iter -= Vec2(target->getContentSize().width / 2, 0);
 				(*iter).scale(scaleFactor);
-				*iter += Vec2(target.getContentSize().width / 2, 0);
+				*iter += Vec2(target->getContentSize().width / 2, 0);
 			}
 
 			for (int i = 1; i < curVertices.size(); i++)
 			{
-				//분리축 데이터 initialize
 				if (tAxises.size() < i)
 					tAxises.push_back(new SeparateAxis(curVertices[i - 1], curVertices[i]));
 				else
 					tAxises[i - 1]->initAxis(curVertices[i - 1], curVertices[i]);
 
-				//각 vertex 순회하며, 분리축에 대한 target의 projection 범위 미리 계산
+				//각 분리축에 대해, target의 projection 범위 미리 계산(방향 )
 				auto iter = curVertices.begin();
 				float cur_val = tAxises[i - 1]->getProjection(*iter);
 				tAxises[i - 1]->setProjMin(cur_val);
@@ -134,21 +191,15 @@ public:
 
 				while (++iter != curVertices.end())
 				{
-					cur_val = tAxises[i - 1]->getProjection(*iter);
-
-					//축에 projection 한 값의 min-max 범위를 갱신한다 
-					if (cur_val < tAxises[i - 1]->getProjMinTarget()) {
+					cur_val = tAxises[i - 1]->getProjection(*iter);	
+					if (cur_val < tAxises[i - 1]->getProjMin())
 						tAxises[i - 1]->setProjMin(cur_val);
-					}
-					else if (cur_val > tAxises[i - 1]->getProjMaxTarget()) {
+					if (cur_val > tAxises[i - 1]->getProjMax()) 
 						tAxises[i - 1]->setProjMax(cur_val);
-					}
 				}
 			}
 		}
 	}
-
-
 	inline void drawOutline() {
 		tLiner->clear();
 		for (int i = 0; i < curVertices.size(); i++) {
@@ -156,25 +207,21 @@ public:
 		}
 	}
 
-	/*
-	1. 충돌 여부만 탐지 필요 시, detectCollision
-	2. SAT검사를 통해, 분리 Vec2이 필요하면 detectCollisionBounce
-	*/
-	inline bool detectCollision(Vec2 invade_world_pos, float invade_world_radius)
+	//충돌 여부 탐지 
+	inline bool detectCollision(Vec2 obj_world_pos, float obj_world_radius)
 	{
-		Vec2 opt_pos = target.convertToNodeSpace(invade_world_pos);
-		float opt_radius = invade_world_radius * target.getScale();
+		Vec2 trans_pos = target->convertToNodeSpace(obj_world_pos);
+		float trans_radius = obj_world_radius * target->getScale();
 
 		//모든 분리축에 대해, gap 발생여부 테스트
 		auto iter = tAxises.begin();
 		do
 		{
-			float proj_val = (*iter)->getProjection(opt_pos);
-			float proj_min = proj_val - opt_radius;
-			float proj_max = proj_val + opt_radius;
-
-			//gap 발생 시, 충돌 없음 보장할 수 있음
-			if (proj_max < (*iter)->getProjMinTarget() || (*iter)->getProjMaxTarget() < proj_min)
+			float proj_val = (*iter)->getProjection(trans_pos);
+			float proj_min = proj_val - trans_radius;
+			float proj_max = proj_val + trans_radius;
+			//gap 발생 시, 충돌없음 보장 가능
+			if (proj_max < (*iter)->getProjMin() || (*iter)->getProjMax() < proj_min)
 				return false;
 
 		} while (++iter != tAxises.end());
@@ -182,38 +229,139 @@ public:
 		return true;
 	};
 
-
-
-	inline Vec2 detectCollisionBounce(Vec2 invade_world_pos, float invade_world_radius)
+	//Minimum Separate Axis와 depth 구간 반환
+	inline pair<float, float> getMinPenetrateBound(Vec2 invade_direction, Vec2 center_pos, float radius, Vec2& norm_axis)
 	{
-		Vec2 opt_pos = target.convertToNodeSpace(invade_world_pos);
-		float opt_radius = invade_world_radius * target.getScale();
+		Vec2 opt_pos = target->convertToNodeSpace(center_pos);
+		float opt_radius = radius * target->getScale();
 
 		int min_idx = 0;
-		float min_overlapped = 0.0f;
-		float cur_overlapped = 0.0f;
+		float min_depth = 0.0f;
+		float cur_depth = 0.0f;
 
 		//각 분리축 순회하며, overlap의 크기가 가장 작은 분리축과 값을 추출
 		for (int i = 0; i < tAxises.size(); i++)
 		{
-			if ((cur_overlapped = tAxises[i]->calOverlapped(opt_pos, opt_radius)) == 0.0f)
-				return Vec2::ZERO;
-			if (min_overlapped != 0.0f) {
-				if (cur_overlapped < min_overlapped) {
-					min_overlapped = cur_overlapped;
+			//분리축에 대해 gap 발생 시, zero vec2 반환하며 함수 종료
+			if ((cur_depth = tAxises[i]->getPenetrationDepth(invade_direction, opt_pos, opt_radius)) == 0.0f)
+				return{ 0, 0 };
+
+			if (min_depth != 0.0f) {
+				if (fabsf(cur_depth) < fabsf(min_depth)) {
+					min_depth = cur_depth;
 					min_idx = i;
 				}
 			}
 			else {
-				min_overlapped = cur_overlapped;
+				min_depth = cur_depth;
 				min_idx = i;
 			}
 		}
-		//가장 작은 계수에 대한 분리축 벡터 반환
-		return tAxises[min_idx]->getNormAxis() * min_overlapped;
+
+		norm_axis = convertToWorldSpace(tAxises[min_idx]->getNormAxis());
+		return tAxises[min_idx]->getPenetrationBound(invade_direction, center_pos, radius);
+	}
+	inline pair<float, float> getMinPenetrateBound(Vec2 invade_direction, vector<Vec2>& invade_vertices, Vec2& norm_axis)
+	{
+		vector<Vec2> opt_vertices;
+
+		auto iter = invade_vertices.begin();
+		while (iter != invade_vertices.end()) {
+			opt_vertices.push_back(target->convertToNodeSpace(*iter));
+			iter++;
+		}
+		int min_idx = 0;
+		float min_depth = 0.0f;
+		float cur_depth = 0.0f;
+
+		//각 분리축 순회하며, overlap의 크기가 가장 작은 분리축과 값을 추출
+		for (int i = 0; i < tAxises.size(); i++)
+		{
+			//분리축에 대해 gap 발생 시, zero vec2 반환하며 함수 종료
+			if ((cur_depth = tAxises[i]->getPenetrationDepth(invade_direction, invade_vertices)) == 0.0f) {
+				return{ 0, 0 };
+			}
+
+			if (min_depth == 0.0f){
+				min_depth = cur_depth;
+				min_idx = i;
+			}
+			else{
+				if (fabsf(cur_depth) < fabsf(min_depth)) {
+					min_depth = cur_depth;
+					min_idx = i;
+				}
+			}
+		}
+		norm_axis = target->convertToWorldSpace(tAxises[min_idx]->getNormAxis());
+		return tAxises[min_idx]->getPenetrationBound(invade_direction, invade_vertices);
 	}
 
-};
+	inline Vec2 getMinPenetrateVec(Vec2 invade_direction, Vec2 center_pos, float radius)
+	{
+		Vec2 opt_pos = target->convertToNodeSpace(center_pos);
+		float opt_radius = radius * target->getScale();
 
+		int min_idx = 0;
+		float min_depth = 0.0f;
+		float cur_depth = 0.0f;
+
+		//각 분리축 순회하며, overlap의 크기가 가장 작은 분리축과 값을 추출
+		for (int i = 0; i < tAxises.size(); i++)
+		{
+			//분리축에 대해 gap 발생 시, zero vec2 반환하며 함수 종료
+			if ((cur_depth = tAxises[i]->getPenetrationDepth(invade_direction, opt_pos, opt_radius)) == 0.0f)
+				return Vec2::ZERO;
+
+			if (min_depth != 0.0f) {
+				if (fabsf(cur_depth) < fabsf(min_depth)) {
+					min_depth = cur_depth;
+					min_idx = i;
+				}
+			}
+			else {
+				min_depth = cur_depth;
+				min_idx = i;
+			}
+		}
+		CCLOG("penetrate [%.2f, %.2f]", (tAxises[min_idx]->getNormAxis() * min_depth).x, (tAxises[min_idx]->getNormAxis() * min_depth).y);
+		return tAxises[min_idx]->getNormAxis() * min_depth;
+	}
+	inline Vec2 getMinPenetrateVec(Vec2 invade_direction, vector<Vec2>& invade_vertices)
+	{
+		vector<Vec2> opt_vertices;
+
+		auto iter = invade_vertices.begin();
+		while (iter != invade_vertices.end()) {
+			opt_vertices.push_back(target->convertToNodeSpace(*iter));
+		}
+
+		int min_idx = 0;
+		float min_depth = 0.0f;
+		float cur_depth = 0.0f;
+
+
+		//각 분리축 순회하며, overlap의 크기가 가장 작은 분리축과 값을 추출
+		for (int i = 0; i < tAxises.size(); i++)
+		{
+			//분리축에 대해 gap 발생 시, zero vec2 반환하며 함수 종료
+			if ((cur_depth = tAxises[i]->getPenetrationDepth(invade_direction, invade_vertices)) == 0.0f)
+				return Vec2::ZERO;
+
+			if (min_depth != 0.0f) {
+				if (fabsf(cur_depth) < fabsf(min_depth)) {
+					min_depth = cur_depth;
+					min_idx = i;
+				}
+			}
+			else {
+				min_depth = cur_depth;
+				min_idx = i;
+			}
+		}
+		CCLOG("penetrate [%.2f, %.2f]", (tAxises[min_idx]->getNormAxis() * min_depth).x, (tAxises[min_idx]->getNormAxis() * min_depth).y);
+		return tAxises[min_idx]->getNormAxis() * min_depth;
+	}
+};
 
 #endif // !__COLLISION_DETECTOR__
